3417a53872bc2baa64fed8ce09e1edc1fe1d3019
programacion <programacion@evolis3d.com>
2021-03-10 11:11:36 +0100

    Upgrade complete, new features!
    Put everything on a single file.
    
    Added a custom GUI dialog window, generated from https://scriptui.joonas.me
    
    Added support for Text Layers, will convert to TMP_Text elements.
    
    Added support for basic text alignment read from Text Layers's internal properties.
    
    Added support for special prefix "btn_" and "text_", will convert them to those UI elements in Unity when the option is enabled.
    
    Added support for using Photoshop's Internal color-code presets, will use them instead of random color when the option is enabled.
    
    README is also updated witht he new features.
    
    And some more tweaks I can't remember right now...
    
    TO-DO: Shorten the amount of code by enabling colors and prefixes by default...

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ by **Carlos Lecina**
 
 * * *
 
-**PhotoLayout** is a set of tools _(probably more than one script)_ that will export your current PSD document to a self-generated, custom Unity C# script.
-This script is really a GUI generator and composer, a custom editor tool that will create a nested Canvas from scratch, filled with all its children.
+**PhotoLayout** is a single file .jsx Photoshop Script _(a big one!)_ that will export your current PSD document to a self-generated, custom Unity C# script.
+The script is really a GUI generator and a composer, a custom editor tool that will create a nested Canvas from scratch, filled with all its children.
 
 The order of the elements is reversed so it matches the one from your PSD document.
 
@@ -19,18 +19,27 @@ The order of the elements is reversed so it matches the one from your PSD docume
 
 ### Features
 
--   0 dependencies, just the export scripts.
+-   **NEW!** Now comes with a GUI! _(only for Photoshop CC or later)_
+-   **NEW!** Support for special prefixes _(btn_ & text_)_
+-   **NEW!** Option to convert Text Layers into TextMeshPRO Text controls.
+-   **NEW!** Basic text alignment for Text Layers.
+-   **NEW!** Option to color your elements using Photoshop's internal layer-color presets.
+-   0 dependencies, single file.
 -   Modular, so it can be extended in the future. 
 -   C# code generation from scratch, zero, nada.
 -   Uses % percentages and RectTransform' Anchors instead of pixels.
 -   1:1 Replication _(WYSIWYG)_
--   Basic colors for each UI element.
 -   **Extra!** Generates a UIController script. _(if you'll ever need one...)_ 
--   more coming soon...
+-   A few more coming soon...
 
 ### Installation
 
-Just run the included **PhotoLayout.jsx** script in Photoshop to generate the .cs file. When asked, point to your _Project\Assets\Editor_ folder.
+Just run the included **PhotoLayout.jsx** script in Photoshop. Versions CS6 or previous can't show the Settings Dialog window.
+
+When 'Use Layer color-presets' option is turned off, random colors will be used for each element.
+When 'Generate Extras' option is turned off, all Layers will be treated as Image elements.
+ 
+**NOTE:** Ensure to point to your _Project\Assets\Editor_ folder.
 
 Then open Unity3D and execute the Editor script. This will generate the whole Canvas hierarchy and place every item in the Canvas.
 
@@ -41,11 +50,9 @@ There's some weird bug that prevents the line **"using UnityEngine;"** for being
 _(I'm working on it!)_
 
 ### Coming Soon
-- Accurate coloring for UI elements.
 - Layer Groups.
-- Text Layers → TextMeshPRO conversion support.
-- More align features _(I hope so...)_
-- Code clean-up & reorganization _(it's a mess! XD)_
+- More align features _(vertical align options...)_
+- Code clean-up & reorganization _(NOW it's a mess! XD)_
 
 ### Deprecated
 - Export & pack all graphics into a TextureAtlas. _(2D PSD Importer package already does that)_
--- photo4.jsx
+++ photo4.jsx
@@ -25,6 +25,7 @@ var h = ymin + ymax;
 var coords = "";
 
 //GLOBALS PARSER
+var fileLineFeed = "Windows";
 var docName = "editor_GenerateUI.cs";
 var enter = "\n";
 var tabs = "\t";
@@ -33,10 +34,10 @@ var antibarra = "\\";
 var decimal = "f ";
 
 //GLOBALS EXPORT Settings
-var alert_cs6mode = "Photoshop CS6 or previous detected. Random Colors: ON, Add Controls:ON";
+var alert_cs6mode = "Photoshop CS6 or previous detected. Layer Colors: OFF, Add Controls:ON";
 var alert_ccmode = "Photoshop CC or next detected. All features ON";
 
-var OPTIONS_RANDOMCOLORS = false;
+var OPTIONS_LAYERCOLORS = false;
 var OPTIONS_ADDCONTROLS = false;
 var OPTIONS_PATH = null;
 
@@ -49,9 +50,10 @@ function Main() {
 
   if (numversion < 14) {
     alert(alert_cs6mode);
+    OPTIONS_LAYERCOLORS = false;
+    OPTIONS_ADDCONTROLS = true;
     SetPath();
     Generate();
-    //FinalCheck();
   }
   //if numversion >13 , es que usamos una versión CC más nueva...
   else {
@@ -78,7 +80,7 @@ function FinalCheck() {
   if (OPTIONS_PATH == null) {
     alert("Export Aborted", "Canceled");
   } else {
-    alert("Exported " + layernum + " elements successfully to " + OPTIONS_PATH + "/" + docName);
+    alert("Exported " + layerNum + " elements successfully to " + OPTIONS_PATH + "/" + docName);
   }
 }
 
@@ -120,9 +122,9 @@ function ShowGUI() {
   });
   button1.text = "...";
 
-  button1.onClick = function () {
+  button1.onClick = function() {
     SetPath();
-    edittext1.text = OPTIONS_PATH;
+    edittext1.text = decodeURI(OPTIONS_PATH); //OPTIONS_PATH;
   }
 
 
@@ -135,15 +137,19 @@ function ShowGUI() {
     name: "checkbox1"
   });
   checkbox1.helpTip = "Every layer will have its own color";
-  checkbox1.text = "Use Random Colors";
-  OPTIONS_RANDOMCOLORS = checkbox1.value;
+  checkbox1.text = "Use Layer color-presets";
+  checkbox1.onClick = function() {
+    OPTIONS_LAYERCOLORS = checkbox1.value;
+  }
 
   var checkbox2 = dialog.add("checkbox", undefined, undefined, {
     name: "checkbox2"
   });
   checkbox2.helpTip = "Convert btn_ to Button, txt_ for TextMeshPro";
   checkbox2.text = "Generate Extras";
-  OPTIONS_ADDCONTROLS = checkbox2.value;
+  checkbox2.onClick = function() {
+    OPTIONS_ADDCONTROLS = checkbox2.value;
+  }
 
   var divider3 = dialog.add("panel", undefined, undefined, {
     name: "divider3"
@@ -156,13 +162,12 @@ function ShowGUI() {
   button2.text = "Export";
   button2.alignment = ["center", "top"];
 
-  button2.onClick = function () {
-      if (OPTIONS_PATH == null) {
-        alert("Error! File path not specified!");
-      }
-      else {
-        Generate();
-      }
+  button2.onClick = function() {
+    if (OPTIONS_PATH == null) {
+      alert("Error! File path not specified!");
+    } else {
+      Generate();
+    }
   }
 
   //llamada
@@ -170,12 +175,140 @@ function ShowGUI() {
 }
 
 
-function Generate()
-{
+function Generate() {
   //cosas
+  GeneraHeader();
+  recurseLayers(docRef);
+  GeneraFoo();
+  preferences.rulerUnits = defaultRulerUnits;
+  writeFile(coords);
+  //limpio coords y genero el otro fichero, el GeneraController
+  GeneraController();
+  writeFile(coords);
+  FinalCheck();
 }
 
 
+function recurseLayers(currLayers) {
+  for (var i = 0; i < currLayers.layers.length; i++) {
+
+    layerRef = currLayers.layers[i];
+    xmin = layerRef.bounds[0].value;
+    ymin = layerRef.bounds[1].value;
+    xmax = layerRef.bounds[2].value;
+    ymax = layerRef.bounds[3].value;
+    xmin = parseInt(xmin);
+    ymin = parseInt(ymin);
+    xmax = parseInt(xmax);
+    ymax = parseInt(ymax);
+    w = xmin + xmax;
+    w = ymin + ymax;
+
+    if (layerRef.visible == true) {
+      GenerateElement(layerRef);
+    }
+    //ZONA WIP para añadir la feature de Grupo de Capas..
+    //
+  }
+}
+
+function GenerateElement(miLayer) {
+  //
+  var xminu = xmin / screenwidth;
+  var xmaxu = xmax / screenwidth;
+  var yminu = (screenheight - ymin) / screenheight;
+  var ymaxu = (screenheight - ymax) / screenheight;
+  xminu = (xminu).toFixed(3);
+  xmaxu = (xmaxu).toFixed(3);
+  yminu = (yminu).toFixed(3);
+  ymaxu = (ymaxu).toFixed(3);
+  //
+  coords += enter;
+  if (isTrueText(miLayer)) {
+    coords += tabs + "el = new GameObject(" + comillas + "text_" + miLayer.name + comillas + ",";
+  } else {
+    coords += tabs + "el = new GameObject(" + comillas + miLayer.name + comillas + ", ";
+  }
+  if (isButton(miLayer)) {
+    coords += "typeof(Button) ";
+  } else if (isTrueText(miLayer)) {
+    coords += "typeof(TMP_Text) ";
+    var alignText = miLayer.textItem.justification;
+    var texContents = miLayer.textItem.contents;
+  } else if (isText(miLayer)) {
+    coords += "typeof(TMP_Text) ";
+  } else {
+    coords += "typeof(Image) ";
+  }
+  coords += ");" + enter;
+  coords += tabs + "el.transform.SetParent(CanvasRoot.transform);" + enter;
+  coords += tabs + "el.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;" + enter;
+  //POS
+  coords += enter;
+  coords += tabs + "el.GetComponent<RectTransform>().anchorMin = new Vector2(" + xminu + decimal + "," + ymaxu + decimal + ");" + enter;
+  coords += tabs + "el.GetComponent<RectTransform>().anchorMax = new Vector2(" + xmaxu + decimal + "," + yminu + decimal + ");" + enter;
+  coords += tabs + "el.GetComponent<RectTransform>().sizeDelta = Vector2.zero;" + enter;
+  //ALIGN
+  if (isTrueText(miLayer)) {
+    coords += tabs + "el.GetComponent<TMP_Text>().text = " + texContents + ";" + enter;
+    coords += tabs + "el.GetComponent<TMP_Text>().enableAutoSizing = true;" + enter;
+    coords += tabs + "el.GetComponent<TMP_Text>().alignment = " + convertAlignment(alignText) + ";" + enter;
+  }
+  //COLOR
+  if (isTrueText(miLayer) == false) {
+    GenColors(OPTIONS_LAYERCOLORS);
+  }
+  //
+  coords += tabs + "el.transform.SetSiblingIndex(0);" + enter;
+}
+
+function GenColors(myGlobalOption) {
+  if (myGlobalOption == false) {
+    if (layerRef.isBackgroundLayer) {
+      coords += tabs + "el.GetComponent<Image>().color = Color.white;" + enter;
+    } else {
+      var redCol = Math.floor(Math.random() * 256);
+      var greCol = Math.floor(Math.random() * 256);
+      var bluCol = Math.floor(Math.random() * 256);
+      coords += tabs + "el.GetComponent<Image>().color = new Color32(" + redCol + "," + greCol + "," + bluCol + ",255);" + enter;
+    }
+  } else {
+    var strColor = getLayerColour();
+    // "none","red","orange","yellowColor","grain","blue","violet","gray"
+    if (strColor == "none") coords += tabs + "el.GetComponent<Image>().color = Color.white;" + enter;
+    else if (strColor == "red") coords += tabs + "el.GetComponent<Image>().color = Color.red;" + enter;
+    //else if (strColor == "orange") coords += tabs + "el.GetComponent<Image>().color = Color.white;" + enter;
+    else if (strColor == "yellowColor") coords += tabs + "el.GetComponent<Image>().color = Color.yellow;" + enter;
+    else if (strColor == "grain") coords += tabs + "el.GetComponent<Image>().color = Color.green;" + enter;
+    else if (strColor == "blue") coords += tabs + "el.GetComponent<Image>().color = Color.blue;" + enter;
+    //else if (strColor == "violet") coords += tabs + "el.GetComponent<Image>().color = Color.white;" + enter;
+    else if (strColor == "gray") coords += tabs + "el.GetComponent<Image>().color = Color.gray;" + enter;
+  }
+}
+
+function convertAlignment(myAlign) {
+  var rsult = "TextAlignmentOptions.Left";
+  if (myAlign == Justification.LEFT) rsult = "TextAlignmentOptions.Left";
+  else if (myAlign == Justification.CENTER) rsult = "TextAlignmentOptions.Center";
+  else if (myAlign == Justification.RIGHT) rsult = "TextAlignmentOptions.Right";
+  return rsult;
+}
+
+function isTrueText(thisLayer) {
+  return (thisLayer.kind == LayerKind.TEXT && OPTIONS_ADDCONTROLS == true);
+}
+
+function isText(thisLayer) {
+  var miniT = thisLayer.name.toUpperCase();
+  miniT = miniT.substring(0, 5);
+  return (miniT == "TEXT_" && OPTIONS_ADDCONTROLS == true);
+}
+
+function isButton(thisLayer) {
+  var miniL = thisLayer.name.toUpperCase();
+  miniL = miniL.substring(0, 4);
+  return (miniL == "BTN_" && OPTIONS_ADDCONTROLS == true);
+}
 
 function GeneraHeader() {
   coords += enter;
@@ -188,6 +321,9 @@ function GeneraHeader() {
   cooods += "using UnityEngine;" + enter;
   coords += "using UnityEngine.UI;" + enter;
   coords += "using UnityEditor;" + enter;
+  if (OPTIONS_ADDCONTROLS == true) {
+    coords += "using TMPro;" + enter;
+  }
   coords += enter;
 
   //
@@ -239,11 +375,64 @@ function GeneraFoo() {
   coords += enter;
 }
 
+function GeneraController() {
+  //1. Overwrite some system vars..
+  coords = "";
+  docName = "UIController.cs";
+  //2. the script..
+  coords += enter;
+  coords += "//TEMPORAL FIX: Please, include the UnityEngine; line to this code by yourself, in case it's not there..." + enter;
+  coords += enter;
+  coords += enter;
+  coords += "using System.Collections;" + enter;
+  coords += "using System.Collections.Generic;" + enter;
+  cooods += "using UnityEngine;" + enter;
+  coords += "using UnityEngine.UI;" + enter;
+  coords += "using UnityEditor;" + enter;
+  if (OPTIONS_ADDCONTROLS == true) {
+    coords += "using TMPro;" + enter;
+  }
+  coords += enter;
+  //
+  coords += "public class UIController : MonoBehaviour" + enter;
+  coords += "{" + enter;
+  //
+  coords += tabs + "public Canvas canv;" + enter;
+  coords += tabs + "private CanvasGroup canvGroup;" + enter;
+  coords += enter;
+  coords += tabs + "[Header(" + comillas + "UI Elements" + comillas + ")]" + enter;
+  //each layer avaiable. NOTE: This is common type variables, I won't specify data type for them...
+  for (var i = 0; i < app.activeDocument.layers.length; i++) {
+    layerRef = app.activeDocument.layers[i];
+    if (layerRef.visible == true) {
+      if (isTrueText(layerRef)) {
+        coords += tabs + "public GameObject " + "text_" + layerRef.name + ";" + enter;
+      } else {
+        coords += tabs + "public GameObject " + layerRef.name + ";" + enter;
+      }
+    }
+  }
+  //
+  coords += "}" + enter;
+  coords += enter;
+}
+
+function writeFile(info) {
+  try {
+    var f = new File(OPTIONS_PATH + "/" + docName);
+    f.remove();
+    f.open('a');
+    f.lineFeed = fileLineFeed;
+    f.write(info);
+    f.close();
+  } catch (e) {}
+}
+
 function getLayerColour() {
   //Colours returned ....
   // "none","red","orange","yellowColor","grain","blue","violet","gray"
   var ref = new ActionReference();
-  ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+  ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
   var appDesc = executeActionGet(ref);
-  return typeIDToStringID(appDesc.getEnumerationValue(stringIDToTypeID('color')) );
+  return typeIDToStringID(appDesc.getEnumerationValue(stringIDToTypeID('color')));
 }
